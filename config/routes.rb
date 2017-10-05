b08aad8e9b4f6ddf77f7199988048d0ac66ae2fd
Helton <helton.dias.rodrigues@gmail.com>
2017-10-05 13:26:16 +0000

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  
+  root "pages#home"
+  
+  get '/about', to:"pages#about"
+  get '/help', to:"pages#help"
+
+
+
+  resources :todos
+  resources :photos
+  
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
